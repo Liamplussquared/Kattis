7de7391f9f6e346f3244b180147425d72eef6cfc
Liam O' Brien <32780409+Liamplussquared@users.noreply.github.com>
2018-09-20 19:32:38 +0100

    Add files via upload

---
+++ Python/Modulo.py
@@ -0,0 +1,13 @@
+#https://open.kattis.com/problems/modulo
+uniqueNumbers = []
+count = 0
+for i in range(10):
+    n = input()
+    modulo = int(n)%42
+    if modulo not in uniqueNumbers:
+        count +=1
+        uniqueNumbers.append(modulo)
+print(count)
+
+        
+
