fcaac48439e3124782667cc09ed507c80c597d09
Liam O' Brien <32780409+Liamplussquared@users.noreply.github.com>
2018-09-27 20:37:36 +0100

    Add files via upload

---
+++ Python/NewAlphabet.py
@@ -0,0 +1,36 @@
+#https://open.kattis.com/problems/anewalphabet
+alphabet = {
+    "A": "@",
+    "B": "8",
+    "C": "(",
+    "D": "|)",
+    "E": "3",
+    "F": "#",
+    "G": "6",
+    "H": "[-]",
+    "I": "|",
+    "J": "_|",
+    "K": "|<",
+    "L": "1",
+    "M": "[]\\/[]",
+    "N": "[]\\[]",
+    "O": "0",
+    "P": "|D",
+    "Q": "(,)",
+    "R": "|Z",
+    "S": "$",
+    "T": "']['",
+    "U": "|_|",
+    "V": "\\/",
+    "W": "\\/\\/",
+    "X": "}{",
+    "Y": "`/",
+    "Z": "2",
+    " ":" "
+}
+translation = ""
+for letter in input():
+    if letter.upper() in alphabet:
+        translation+= alphabet[letter.upper()]
+    else: translation+=letter
+print (translation)
